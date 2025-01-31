a0c3e96ed29fcf23d32270c8262b1385afc3abe5
Daniel Gospodinow <danielgospodinow@gmail.com>
2025-01-31 07:19:14 +0000

    Revert "Add type to untyped var"
    This reverts commit 33e8420e94129ac0bbae8378050c3a7622ca23f1.

--- src/utils/images.ts
+++ src/utils/images.ts
@@ -71,7 +71,7 @@ export const adaptOpenGraphImages = async (
           };
         }
 
-        let _image: any;
+        let _image;
 
         if (
           typeof resolvedImage === 'string' &&
